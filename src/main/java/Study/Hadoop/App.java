29f59780f750925284805e57b52ddb16576abb3e
gyb333 <876368840@qq.com>
2018-08-24 21:21:11 +0800

    Initial commit

---
+++ src/main/java/Study/Hadoop/App.java
@@ -0,0 +1,13 @@
+package Study.Hadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
