579fdcbcf43984f61d9fce109e24933b49682d02
gyb333 <876368840@qq.com>
2018-08-25 23:26:13 +0800

    test

--- src/main/java/Hadoop/MapReduce/WordCount.java
+++ src/main/java/Hadoop/MapReduce/WordCount.java
@@ -9,20 +9,61 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.log4j.PropertyConfigurator;
  
+
+
+
 
 public class WordCount {
 
+	public static Configuration getConfiguration() {
+		Configuration conf =new Configuration();
+		conf.set("fs.defaultFS", "hdfs://ns");
+		conf.set("dfs.nameservices", "ns");
+		conf.set("dfs.ha.namenodes.ns" , "nn1,nn2");
+		conf.set("dfs.namenode.rpc-address.ns.nn1","hdfs://Master:9000");
+		conf.set("dfs.namenode.rpc-address.ns.nn2" ,"hdfs://Second:9000");
+		conf.set("dfs.client.failover.proxy.provider.ns","org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider");
+		
+//		conf.set("mapreduce.framework.name", "yarn");
+//		conf.set("yarn.resourcemanager.cluster-id", "yrc");
+//		conf.set("yarn.resourcemanager.ha.rm-ids", "rm1,rm2");
+
+//		conf.set("yarn.resourcemanager.hostname.rm1", "Master");
+//		conf.set("yarn.resourcemanager.hostname.rm2", "Master");
+//		conf.set("yarn.client.failover-proxy-provider", "org.apache.hadoop.yarn.client.ConfiguredRMFailoverProxyProvider");
+		
+		return conf;
+	}
+	
+	static {
+	    try {
+	    	//强制加载dll
+	    	System.load("D:\\work\\hadoop-3.0.0\\bin\\hadoop.dll");
+	    } catch (UnsatisfiedLinkError e) {
+	      System.err.println("Native code library failed to load.\n" + e);
+	      System.exit(1);
+	    }
+	  }
+	
+	
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		
-		Configuration conf =new Configuration();
+		String path =System.getProperty("user.dir")+"\\Resources\\";
+		PropertyConfigurator.configure(path+"log4j.properties");
+		
+		//设置环境变量
+		System.setProperty("hadoop.home.dir", "D:\\work\\hadoop-3.0.0");
+		
+		Configuration conf =getConfiguration();
 		//设置job运行要访问的默认文件系统
-		conf.set("fs.defaultFS", "hdfs://Master:9000");
+	 
+		
 		//设置job提交到哪运行
-		conf.set("mapreduce.framework.name", "yarn");
-		conf.set("yarn.resourcemanager.hostname", "Master");
 		
-		
+		 
+		 
 		
 		//设置Job运行时系统参数
 		System.setProperty("HADOOP_USER_NAME", "root");
@@ -32,8 +73,11 @@ public class WordCount {
 		
 		Job job =Job.getInstance(conf);
 		
+		//job.setJar("D:\\work\\Hadoop\\target\\Hadoop-0.0.1-SNAPSHOT.jar");
 		//设置job获取jar包的位置
 		job.setJarByClass(WordCount.class);
+		
+		
 		
 		job.setMapperClass(WordCountMapper.class);
 		job.setReducerClass(WordCountReducer.class);
@@ -45,7 +89,7 @@ public class WordCount {
 		job.setOutputValueClass(IntWritable.class);
 		
 		FileInputFormat.setInputPaths(job, new Path("/wordcount/input"));
-		FileOutputFormat.setOutputPath(job, new Path("/wordcount/output"));
+		FileOutputFormat.setOutputPath(job, new Path("/wordcount/output/2/"));
 		
 		job.setNumReduceTasks(3);
 		
