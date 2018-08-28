4650b2fd3ba1e6a983d6581d4cf382463dbf0cb7
gyb333 <876368840@qq.com>
2018-08-28 22:46:19 +0800

    test

---
+++ MapReduce/src/main/java/Study/MapReduce/Test.java
@@ -0,0 +1,28 @@
+package Study.MapReduce;
+
+import org.apache.hadoop.conf.Configuration;
+
+public class Test {
+
+	public static void TestConfiguration() {
+		
+		Configuration conf =new Configuration();
+		conf.addResource(Test.class.getResourceAsStream("/Param.xml"));
+		
+		 //‘O÷√›î»Î›î≥ˆâ∫øs
+		conf.setBoolean("mapreduce.map.output.compress", true);
+		conf.set("mapreduce.map.output.compress.codec", "org.apache.hadoop.io.compress.DefaultCodec");
+		
+		conf.setBoolean("mapreduce.output.fileoutputformat.compress", true);
+		conf.set("mapreduce.output.fileoutputformat.compress.type", "RECORD");
+		conf.set("mapreduce.output.fileoutputformat.compress.codec", "org.apache.hadoop.io.compress.DefaultCodec");
+		String value =conf.get("name");
+		System.out.println(value);
+	}
+	
+	
+	public static void main(String[] args) {
+		TestConfiguration();
+	}
+
+}
