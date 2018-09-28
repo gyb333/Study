a7dd1130563c743c29115bd63642d3908185a75e
gyb333 <876368840@qq.com>
2018-09-29 00:20:21 +0800

    tet

---
+++ ScalaSpark/src/main/scala/Study/ScalaSpark/Test.scala
@@ -0,0 +1,8 @@
+package Study.ScalaSpark
+
+object Test {
+  def main(args:Array[String]):Unit = {
+    
+    println("hello world")
+  }
+}
---
+++ ScalaSpark/src/main/scala/Study/ScalaSpark/WordCount.scala
@@ -0,0 +1,22 @@
+package Study.ScalaSpark
+
+import org.apache.spark.{SparkConf,SparkContext}
+import org.apache.spark.rdd.{RDD}
+
+object WordCount {
+  
+  val inputPath:String=""
+  val outputPath:String=""
+  
+   def main(args:Array[String]):Unit={
+      val conf = new SparkConf().setAppName("WordCount").setMaster("local")
+      val sc =new SparkContext(conf)
+      val lines: RDD[String] =sc.textFile(inputPath) 
+      val words:RDD[String]=lines.flatMap(_.split(" "))
+      val wordAndOne :RDD[(String, Int)]=words.map((_,1))
+      val reduced : RDD[(String, Int)]=wordAndOne.reduceByKey(_+_)
+      val sorted: RDD[(String, Int)] =reduced.sortBy(_._2, false)
+      sorted.saveAsTextFile(outputPath)
+      sc.stop()
+   }
+}
